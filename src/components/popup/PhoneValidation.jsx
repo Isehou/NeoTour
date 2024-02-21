c7da11ea6bd820b0ecb8841975a0c052c20702ed
isehou <zaku47shift@gmail.com>
2024-02-22 00:53:02 +0600

    upgrade code, in proccesse layout on validation form

--- src/components/popup/PhoneValidation.jsx
+++ src/components/popup/PhoneValidation.jsx
@@ -9,6 +9,19 @@ import "./phone-validation.css";
 
 function InfoValidation() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [count, setCount] = useState(0);
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    if (count < 5) {
+      setCount(count + 1);
+    }
+  };
 
   const countries = defaultCountries.filter((country) => {
     const { iso2 } = parseCountry(country);
@@ -26,7 +39,7 @@ function InfoValidation() {
           </p>
         </div>
         <div className="">
-          <p>Phone Number</p>
+          <p className="validation-subtitle">Phone Number</p>
           <PhoneInput
             defaultCountry="kg"
             value={phoneNumber}
@@ -34,6 +47,37 @@ function InfoValidation() {
             countries={countries}
           />
         </div>
+        <div className="">
+          <p className="validation-subtitle">Commentaries to trip</p>
+          <input
+            className="validation-input"
+            type="text"
+            placeholder="Write your wishes to trip..."
+          />
+        </div>
+        <div className="validation-people-preorder">
+          <p className="validation-subtitle">Commentaries to trip</p>
+          <div className="alignment-block">
+            <div className="validation-people-preorder block-grey">
+              <button
+                className="validation-people-preorder__btn"
+                onClick={handleDecrement}
+              >
+                -
+              </button>
+              <p>{count}</p>
+              <button
+                className="validation-people-preorder__btn"
+                onClick={handleIncrement}
+              >
+                +
+              </button>
+            </div>
+            <img src="" alt="" />
+            <p>{count} People</p>
+          </div>
+        </div>
+        <button className="validation-submit__btn">Submit</button>
       </div>
     </div>
   );
